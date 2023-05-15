c1a4b82a7d3dcdce77a82e71233a7e564576bf33
rasbt <mail@sebastianraschka.com>
2023-05-15 12:23:14 -0500

    unit 9 code

--- README.md
+++ README.md
@@ -118,7 +118,7 @@ For other announcements, updates, and additional materials, you can follow [Ligh
 
 
 
-### Unit 8. Introduction to Natural Language Processing and Large Language Models
+### Unit 8. Introduction to Natural Language Processing and Large Language Models  [ [Link to videos](https://lightning.ai/pages/courses/deep-learning-fundamentals/unit-8.0-natural-language-processing-and-large-language-models/) ] 
 
 - 8.1 Working with Text Data
 - [8.2 Training Text Classifier Baseline](unit08-large-language-models/8.2-bag-of-words)
@@ -129,6 +129,12 @@ For other announcements, updates, and additional materials, you can follow [Ligh
 - [8.7 Using Large Language Model for Classification](unit08-large-language-models/8.7-distilbert-finetuning)
 - [Unit 8 exercises](unit08-large-language-models/exercises)
 
-### Unit 9. Coming Soon
+### Unit 9. Speeding Up Model Training
+
+- [9.1 Accelerated Model Training via Mixed-Precision Training](unit09-performance/9.1-mixed-precision)
+- 9.2 Multi-GPU Training Strategies
+- [9.3 Deep Dive Into Data Parallelism](unit09-performance/9.3-multi-gpu)
+- [9.4 Compiling PyTorch Models](unit09-performance/9.4-compile)
+- [9.5 Increasing Batch Sizes to Increase Throughput](unit09-performance/9.5-batchsize-finder)
 
 ### Unit 10. Coming Soon
---
+++ unit09-performance/9.4-compile/compile.py
@@ -0,0 +1,207 @@
+import os
+import os.path as op
+import time
+
+import lightning as L
+import matplotlib.pyplot as plt
+import pandas as pd 
+import torch
+import torchmetrics
+from datasets import load_dataset
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger
+from local_dataset_utilities import (
+    IMDBDataset,
+    download_dataset,
+    load_dataset_into_to_dataframe,
+    partition_dataset,
+)
+from torch.utils.data import DataLoader
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from watermark import watermark
+
+
+# pip install -U deepspeed
+
+def tokenize_text(batch):
+    return tokenizer(batch["text"], truncation=True, padding=True)
+
+
+def plot_logs(log_dir):
+    metrics = pd.read_csv(op.join(log_dir, "metrics.csv"))
+
+    aggreg_metrics = []
+    agg_col = "epoch"
+    for i, dfg in metrics.groupby(agg_col):
+        agg = dict(dfg.mean())
+        agg[agg_col] = i
+        aggreg_metrics.append(agg)
+
+    df_metrics = pd.DataFrame(aggreg_metrics)
+    df_metrics[["train_loss", "val_loss"]].plot(
+        grid=True, legend=True, xlabel="Epoch", ylabel="Loss"
+    )
+    plt.savefig(op.join(log_dir, "loss.pdf"))
+
+    df_metrics[["train_acc", "val_acc"]].plot(
+        grid=True, legend=True, xlabel="Epoch", ylabel="Accuracy"
+    )
+    plt.savefig(op.join(log_dir, "acc.pdf"))
+
+
+class LightningModel(L.LightningModule):
+    def __init__(self, model, learning_rate=5e-5):
+        super().__init__()
+
+        self.learning_rate = learning_rate
+        self.model = model
+
+        self.train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+        self.val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+        self.test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=2)
+
+    def forward(self, input_ids, attention_mask, labels):
+        return self.model(input_ids, attention_mask=attention_mask, labels=labels)
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(
+            batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["label"],
+        )
+        self.log("train_loss", outputs["loss"])
+        with torch.no_grad():
+            logits = outputs["logits"]
+            predicted_labels = torch.argmax(logits, 1)
+            self.train_acc(predicted_labels, batch["label"])
+            self.log("train_acc", self.train_acc, on_epoch=True, on_step=False)
+        return outputs["loss"]  # this is passed to the optimizer for training
+
+    def validation_step(self, batch, batch_idx):
+        outputs = self(
+            batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["label"],
+        )
+        self.log("val_loss", outputs["loss"], prog_bar=True)
+
+        logits = outputs["logits"]
+        predicted_labels = torch.argmax(logits, 1)
+        self.val_acc(predicted_labels, batch["label"])
+        self.log("val_acc", self.val_acc, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        outputs = self(
+            batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["label"],
+        )
+
+        logits = outputs["logits"]
+        predicted_labels = torch.argmax(logits, 1)
+        self.test_acc(predicted_labels, batch["label"])
+        self.log("accuracy", self.test_acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.trainer.model.parameters(), lr=self.learning_rate
+        )
+        return optimizer
+
+
+if __name__ == "__main__":
+
+    print(watermark(packages="torch,lightning,transformers,deepspeed", python=True))
+    print("Torch CUDA available?", torch.cuda.is_available())
+
+    torch.manual_seed(123)
+
+    # #########################
+    # ## 1 Loading the Dataset
+    # #########################
+    download_dataset()
+    df = load_dataset_into_to_dataframe()
+    if not (op.exists("train.csv") and op.exists("val.csv") and op.exists("test.csv")):
+        partition_dataset(df)
+
+    imdb_dataset = load_dataset(
+        "csv",
+        data_files={
+            "train": "train.csv",
+            "validation": "val.csv",
+            "test": "test.csv",
+        },
+    )
+
+    # ########################################
+    # ## 2 Tokenization and Numericalization
+    # #######################################
+
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    print("Tokenizer input max length:", tokenizer.model_max_length)
+    print("Tokenizer vocabulary size:", tokenizer.vocab_size)
+
+    print("Tokenizing ...")
+    imdb_tokenized = imdb_dataset.map(tokenize_text, batched=True, batch_size=None)
+    del imdb_dataset
+    imdb_tokenized.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # ########################################
+    # ## 3 Set Up DataLoaders
+    # ########################################
+
+    train_dataset = IMDBDataset(imdb_tokenized, partition_key="train")
+    val_dataset = IMDBDataset(imdb_tokenized, partition_key="validation")
+    test_dataset = IMDBDataset(imdb_tokenized, partition_key="test")
+
+    train_loader = DataLoader(
+        dataset=train_dataset, batch_size=64, shuffle=True, num_workers=4
+    )
+
+    val_loader = DataLoader(dataset=val_dataset, batch_size=64, num_workers=4)
+
+    test_loader = DataLoader(dataset=test_dataset, batch_size=64, num_workers=4)
+
+    # ########################################
+    # ## 4 Initializing the Model
+    # ########################################
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased", num_labels=2
+    )
+
+    model = torch.compile(model)
+
+    # ########################################
+    # ## 5 Finetuning
+    # ########################################
+
+    lightning_model = LightningModel(model)
+
+    callbacks = [
+        ModelCheckpoint(save_top_k=1, mode="max", monitor="val_acc")  # save top 1 model
+    ]
+    logger = CSVLogger(save_dir="logs/", name="my-model")
+
+    trainer = L.Trainer(
+        max_epochs=3,
+        callbacks=callbacks,
+        precision="16-mixed",
+        accelerator="gpu",
+        devices=[7],
+        logger=logger,
+        log_every_n_steps=10,
+        deterministic=True,
+    )
+
+    start = time.time()
+    trainer.fit(
+        model=lightning_model,
+        train_dataloaders=train_loader,
+        val_dataloaders=val_loader,
+    )
+
+    end = time.time()
+    elapsed = end - start
+    print(f"Time elapsed {elapsed/60:.2f} min")
